d168a4305ec483be06bd8dcc41921f60ee402034
Wenyi.Qian <wenyi.qian@unity.cn>
2024-01-26 11:07:15 +0800

    add things

---
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public static class BuildTool
+{
+    [MenuItem("Build Tool / Build WeixinMinigame")]
+    public static void BuildWeixinMinigame()
+    {
+        var outputDir = Path.Combine(Application.dataPath, "..", "Build");
+        if (Directory.Exists(outputDir))
+            Directory.Delete(outputDir, true);
+        Directory.CreateDirectory(outputDir);
+        
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(scene => scene.path).ToArray(),
+            locationPathName = outputDir,
+            target = BuildTarget.WeixinMiniGame,
+            options = BuildOptions.None
+        };
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes, output: " + outputDir);
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
